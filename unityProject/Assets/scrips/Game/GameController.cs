6d0b8d8c7c64e509b76fc19b004bad3259f93300
VitorFr <v.fr@outlook.com>
2014-04-10 22:39:44 -0300

    Inclusão dos arquivos de porjetos que faltaram na última atualização.

--- unityProject/Assets/scrips/Game/GameController.cs
+++ unityProject/Assets/scrips/Game/GameController.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
+		public GameObject cam;
+		public GameObject hero;
 
 		void Awake ()
 		{
 				Application.targetFrameRate = 60;
 				Debug.Log ("AWAKE");
 		}
+
+		void Update ()
+		{
+				cam.transform.position = new Vector3 (hero.transform.position.x, hero.transform.position.y, -10);
+		}
+
 }
