b92b4df70247856125400e858e192984ee36ffe6
VitorFr <v.fr@outlook.com>
2014-04-10 23:17:10 -0300

    Script do Heroi com alterações para subir escadas

--- unityProject/Assets/scrips/Hero/Hero.cs
+++ unityProject/Assets/scrips/Hero/Hero.cs
@@ -4,29 +4,58 @@ using System.Collections;
 public class Hero : MonoBehaviour
 {
 
+		public Transform hero;
+
+		private Transform groundCheck;		
+
 		private readonly float RUNSPEED = 0.05f;
 
 		private readonly float ROLLSPEED = 0.04f;
 
 		private bool facingRight;
 
+		private bool grounded;
+			
+		private bool run;
+
+		private bool inLadder; 
+
 		private Animator animator;
 
 		// Use this for initialization
 		void Start ()
 		{
+				this.groundCheck = transform.Find ("groundCheck");
 
-				this.animator = GetComponent<Animator> ();
+				this.animator = hero.GetComponent<Animator> ();
 
 				this.facingRight = true;
+
+				this.grounded = false;
+		
+				this.run = false;
+
+				this.inLadder = false;
 	
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				this.roll ();
+				this.grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));				
+				this.inLadder = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ladder"));
+			
+				if (grounded && !inLadder) {		
+						this.roll ();
+				} else if (inLadder && !grounded) {
+						climb ();
+				} else if (!inLadder && !grounded) {
+						this.rigidbody2D.transform.position += new Vector3 (0, -RUNSPEED * 2, 0);
+				}			
+
 				this.move ();
+
+
 		}
 
 		private void roll ()
@@ -47,28 +76,36 @@ public class Hero : MonoBehaviour
 		}
 
 		private void move ()
-		{
+		{		
 				//Running State Control
 				int mult = 0;
+				run = false;
+
 				if (Input.GetKey (KeyCode.RightArrow)) {
-						this.animator.SetBool ("run", true);
+						run = true;
 						if (this.facingRight) {
 								mult = 1;
 						} else {
 								this.flip ();
 						}
 				} else if (Input.GetKey (KeyCode.LeftArrow)) {
-						this.animator.SetBool ("run", true);
+						run = true;
 						if (!this.facingRight) {
 								mult = -1;
 						} else {
 								this.flip ();
 						}
 				}
+					
 				if (Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.LeftArrow)) {
-						this.animator.SetBool ("run", false);
+						run = false;
 				}
-				this.rigidbody2D.transform.position += new Vector3 (RUNSPEED * mult, 0, 0);
+				this.rigidbody2D.transform.position += new Vector3 (RUNSPEED * mult, 0, 0);			 
+				if (!grounded) {
+						run = false;
+				}
+
+				this.animator.SetBool ("run", run);
 		}
 
 		private void flip ()
@@ -78,4 +115,16 @@ public class Hero : MonoBehaviour
 				scale.x *= -1;
 				this.transform.localScale = scale;
 		}
+
+		private void climb ()
+		{
+				int mult = 0;
+				if (Input.GetKey (KeyCode.DownArrow)) {
+						mult = -1;
+				} else if (Input.GetKey (KeyCode.UpArrow)) {
+						mult = 1;
+				}
+				this.rigidbody2D.transform.position += new Vector3 (0, RUNSPEED * mult, 0);
+		}
+
 }
