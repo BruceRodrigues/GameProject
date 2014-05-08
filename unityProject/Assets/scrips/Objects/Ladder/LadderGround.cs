0c16af748d170d97a9229c3c697c78e35e21ac93
VitorFr <v.fr@outlook.com>
2014-05-07 23:52:00 -0300

    Acertos após merge. Adicionada cena SVitor

---
+++ unityProject/Assets/scrips/Objects/Ladder/LadderGround.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LadderGround : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void OnTriggerEnter2D (Collider2D coll)
+		{
+				if (coll.gameObject.layer == LayerEnum.TANGIBLE.number) {
+						//	ITangible tangible = Collider2D.FindObjectOfType<ITangible> ();
+						//tangible.hit (this.damage);
+				}
+		}
+}
