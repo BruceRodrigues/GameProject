6772335208fdfbbbe679494ecb9e2919066e7881
VitorFr <v.fr@outlook.com>
2014-05-07 23:54:32 -0300

    Commit anterior não foi completo

--- unityProject/Assets/scrips/Enums/LayerEnum.cs
+++ unityProject/Assets/scrips/Enums/LayerEnum.cs
@@ -11,14 +11,11 @@ using System;
 public class LayerEnum
 {		
 		public static LayerEnum GROUND = new LayerEnum (8);
-<<<<<<< HEAD
 		public static LayerEnum Ladder = new LayerEnum (9);
 		public static LayerEnum BLadderGround = new LayerEnum (10);
 		public static LayerEnum TANGIBLE = new LayerEnum (11);
 		public static LayerEnum TLadderGround = new LayerEnum (12);
-=======
 		public static LayerEnum NPC = new LayerEnum (12);
->>>>>>> db4ebe4e063e834d6a4164ea884c0608d0194111
 
 		public int number;
 
